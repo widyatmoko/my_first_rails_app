f988443e205ea6af3edf0530cb3d0f329125703e
indrajati widyatmoko <widyatmoko.indrajati@gmail.com>
2018-10-13 02:54:55 -0700

    redoingscaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_13_095342) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.integer "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
